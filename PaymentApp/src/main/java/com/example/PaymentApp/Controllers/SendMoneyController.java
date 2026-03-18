e49bda83c04fc39b7a6caf2f627304a48e81a6fb
Chandu <chanduvalluri22@gmail.com>
2026-03-18 16:02:54 +0530

    service for addnewadmin

--- PaymentApp/src/main/java/com/example/PaymentApp/Controllers/SendMoneyController.java
+++ PaymentApp/src/main/java/com/example/PaymentApp/Controllers/SendMoneyController.java
@@ -1,10 +1,13 @@
 package com.example.PaymentApp.Controllers;
 
 import com.example.PaymentApp.DTO.TransferRequest;
+import com.example.PaymentApp.Entity.User;
+import com.example.PaymentApp.Repositories.UserRepo;
 import com.example.PaymentApp.Service.SendMoneyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +20,19 @@ public class SendMoneyController {
     @Autowired
     private SendMoneyService sendMoneyService;
 
+    @Autowired
+    private UserRepo userRepo;
     @PostMapping
     public ResponseEntity TransferMoney(@RequestBody TransferRequest transferRequest) {
-        boolean Successfultransactionn = sendMoneyService.SendMoney(transferRequest.getAmount(), transferRequest.getUserEmail());
-        if (Successfultransactionn)
-            return  new ResponseEntity<>(HttpStatus.ACCEPTED);
-        return  new ResponseEntity<>(HttpStatus.CONFLICT);
-    }
+        String currentuser = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepo.findByuserName(currentuser);
+        String currentuserEmail = user.getUserEmail();
+        if (transferRequest.getUserEmail() != currentuserEmail) {
+            boolean Successfultransactionn = sendMoneyService.SendMoney(transferRequest.getAmount(), transferRequest.getUserEmail());
+            if (Successfultransactionn)
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
 
 }
